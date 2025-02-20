2c98416a2cf1b92d07e11b535374ca6b289fa0bb
Ainembabazi Lucia Rachel <ainembabaziluciarachel02@gmail.com>
2025-02-20 16:24:19 +0300

    added a new file datatype and modified example file

---
+++ datatypes.py
@@ -0,0 +1,22 @@
+# datatypes specify the type of data that can be stoored inside a variable.
+# data types in python include:
+# numeric(int,float,complex)
+# string
+#mapping
+#bool   
+#set
+#sequuence(list,tuple,range)
+
+num = 200 #integer value.
+num2 = 200.0 #floating value.
+num3 = 1+ 2j #complex value.
+print(type(num))
+print(type(num2))
+print(type(num3))
+word = "helloworld" #string value.
+print(type(word))
+print(word[0])
+print(word[1])
+print(word[2])
+print(word[3])
+
--- example1.py
+++ example1.py
@@ -11,4 +11,11 @@ print(num1+num2)
 # compound variable names should be joined either camel notation/underscore eg. num_1,_num1 or  myNum.
 # avoid using capital letters to begin the name eg. Num1,Num2,Num3 etc.
 # do not use special words as variable names eg. print,int,for,if,input,output...
-# variables in python are cas sensitive eg. num1 is different from Num2.
+# variables in python are case sensitive eg. num1 is different from Num2.
+# A complete instruction to a computer is a statement eg. line 3-line 7.
+# a statement that evaluates to a result ia an expression eg. line 7.
+# a python file is called a python script.
+# a group of statements is called a code.
+# a group of related code is called a block.
+# a block of code in a python script is called a program. eg. example1.py.
+# the running/implementing of a program is called execution. eg.
